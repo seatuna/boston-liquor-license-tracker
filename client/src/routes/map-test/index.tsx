93828657479ac14837858ca80223d2a14a74b046
Celena T. <16471076+seatuna@users.noreply.github.com>
2025-04-29 15:36:43 -0400

    add routing with tanstack router
    - Added tanstack router
    - Moved BostonZipCodeMap to `/map-test/maplibre`
    - Created route for `/map-test`, just contains some text and a link to
    the zip code map built with maplibre
    
    Decided not to co-locate the components with the routes to give us
    freedom on how we want to organize the other files and folders. Since I
    don't think this project will be that big, it shouldn't be a problem.

---
+++ client/src/routes/map-test/index.tsx
@@ -0,0 +1,18 @@
+import { createFileRoute, Link } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/map-test/")({
+  component: MapTestComponent,
+});
+
+function MapTestComponent() {
+  return (
+    <div>
+      <h1>Map Test</h1>
+      <p>
+        This is a test page for the map component. It will be used to test
+        different map libraries and features.
+      </p>
+      <Link to="/map-test/maplibre">MapLibre Boston Zip Codes Map</Link>
+    </div>
+  );
+}
