1337980f05801d60028038126dff99a20fc5f42c
seatuna <chibiynm@gmail.com>
2025-05-05 22:21:38 -0400

    add shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <div className="text-3xl font-bold underline">This is my map App</div>
+      <div className="text-3xl font-bold underline my-5">
+        This is my map App
+      </div>
+      <Button>Test shadcn btn</Button>
     </>
   );
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
@@ -11,4 +12,9 @@ export default defineConfig({
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
     tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
