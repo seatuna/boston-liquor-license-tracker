fea33a3d3fbd73f2d8a49948bc4d272c504d4ef3
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-05-06 15:30:20 -0400

    Set viteconfig base to /boston-liquor-license-tracker/

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/boston-liquor-license-tracker/',
   plugins: [
     // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
