a3987cd3bccbc3ca90bb9e05b24db64dac997a72
JoevaScript1 <131012752+JoevaScript1@users.noreply.github.com>
2025-05-10 11:31:16 -0400

    Header Update
    Added Header with styling and link component criteria.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import Header from "./routes/__root";
 
 function App() {
   return (
     <>
-      <div className="text-3xl font-bold underline my-5">
-        This is my map App
-      </div>
-      <Button>Test shadcn btn</Button>
+      <Header></Header>
     </>
   );
 }
