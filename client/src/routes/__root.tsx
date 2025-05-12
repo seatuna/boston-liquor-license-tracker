af59bef30c2d3919f210d6dc628f93a70d82dce6
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-05-12 17:53:03 -0400

    Create scaffolding for not found page

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import Header from "@/components/layout/header"
 import Footer from "@/components/layout/footer"
+import NotFound from "@/components/pages/not-found/not-found"
 
 export const Route = createRootRoute({
   component: RootComponent,
+  notFoundComponent: NotFound,
 });
 
 function RootComponent() {
