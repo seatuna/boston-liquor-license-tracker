b39621f8a7a3992d2e556668cf4a559e62caadb1
Dan <78275188+rann143@users.noreply.github.com>
2025-05-15 18:19:56 -0400

    Create Hero Image Component (#37)
    **Resolves Ticket #20**
    
    Added Responsive Hero Image Component

--- client/src/components/pages/home/hero.tsx
+++ client/src/components/pages/home/hero.tsx
@@ -1,11 +1,26 @@
 import "./hero.css";
+import logo from "@/assets/logo.svg";
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <h1>Hero</h1>
+    <div className="hero flex flex-col md:items-start md:justify-start w-full sm:w-screen pl-3 pr-3 pt-[100px] pb-[70px] lg:mt-[64px] md:pt-[200px] md:pb-[144px] md:pl-[64px] gap-[24px]">
+      <img src={logo} className="w-2xl lg:w-5xl" alt="Logo" />
+      <h2 className="text-white text-2xl md:text-3xl lg:text-5xl w-full font-bold">
+        Navigating the Liquor License system in Boston is hard. <br /> We are
+        here to make it just a little bit easier.
+      </h2>
+      <p className="text-white text-xs md:text-base">
+        Photo by{" "}
+        <a href="https://unsplash.com/@quinguyen?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          QUI NGUYEN
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/photos/turned-on-filament-bulb-lights-at-bar-counter-Zrp9b3PMIy8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Unsplash
+        </a>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- client/src/components/pages/home/home.tsx
+++ client/src/components/pages/home/home.tsx
@@ -1,15 +1,14 @@
-import "./home.css"
-import Hero from "@/components/pages/home/hero"
-import Navigation from "@/components/pages/home/navigation"
-import Quote from "@/components/pages/home/quote"
-import AboutUs from "@/components/pages/home/about-us"
-import CaseStudy from "@/components/pages/home/case-study"
-import ExtraWork from "@/components/pages/home/extra-work"
+import "./home.css";
+import Hero from "@/components/pages/home/hero";
+import Navigation from "@/components/pages/home/navigation";
+import Quote from "@/components/pages/home/quote";
+import AboutUs from "@/components/pages/home/about-us";
+import CaseStudy from "@/components/pages/home/case-study";
+import ExtraWork from "@/components/pages/home/extra-work";
 
 const Home = () => {
   return (
     <div className="home-page">
-      <h1>Home</h1>
       <Hero />
       <Navigation />
       <Quote />
@@ -17,7 +16,7 @@ const Home = () => {
       <CaseStudy />
       <ExtraWork />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
