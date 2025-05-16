0f9a577637e41782f4a892bdc0f93b925061e497
Ro Desai <desai.ro@gmail.com>
2025-05-16 16:25:03 -0400

    Fill in 'about us' component with content and styling (#38)
    Updated about-us component (tsx and css files) using design info created
    by Griffin.
    Ensured it was responsive and spacing of text was implemented according
    to the spec.
    
    ---------
    
    Co-authored-by: Matthew Clarke <4278212+MattClarke131@users.noreply.github.com>

--- client/src/components/pages/home/about-us.tsx
+++ client/src/components/pages/home/about-us.tsx
@@ -2,8 +2,10 @@ import "./about-us.css";
 
 const AboutUs = () => {
   return (
-    <div className="about-us">
-      <h1>About Us</h1>
+    <div className="about-us w-full">
+      <h1>Who We Are:</h1>
+      <h2 className="w-full break-words whitespace-normal">
+      We are a collection of restaurant owners, bartenders, chefs, and patrons; <b>Just Like You. </b> We understand how hard it is to navigate the red tape of liquor licenses in Boston. From tracking availability to the potential unwritten costs of obtaining one. We've made it our mission to demystify the process to help you get your business off the ground.</h2>
     </div>
   )
 }
