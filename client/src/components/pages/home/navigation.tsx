b98181fdca6e9b092687f3ac8179b28e2197e0a2
Joe <131012752+JoevaScript1@users.noreply.github.com>
2025-05-19 20:07:11 -0400

    Add nav section (#43)
    Nav section completed, adhering to the stylistic guidelines. (did not
    update font family as we should probably serve that CSS publicly?) Will
    add hover/press CSS when provided :)

--- client/src/components/pages/home/navigation.tsx
+++ client/src/components/pages/home/navigation.tsx
@@ -1,11 +1,21 @@
 import './navigation.css';
+import { Link } from "@tanstack/react-router";
+
 
 const Navigation = () => {
   return (
     <div className="navigation">
-      <h1>Navigation</h1>
+      <Link to="/maps">
+        <button className="button-css">MAPS</button>
+      </Link>
+      <Link to="/database">
+        <button className="button-css">LICENSE DATABASE</button>
+      </Link>
+      <Link to="/resources">
+        <button className="button-css">LICENSE RESOURCES</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Navigation
