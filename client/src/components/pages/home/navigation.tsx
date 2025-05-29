a776de39940e28bcfde0cc29a248b35e3b3db8be
Matthew Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-05-29 19:08:57 -0400

    Contain component css to individual css files (#64)
    This change places component css within parent rules.
    This prevents component css from affecting other parts of the page.
    
    This change also removes some unused global css.

--- client/src/components/pages/home/navigation.tsx
+++ client/src/components/pages/home/navigation.tsx
@@ -6,13 +6,13 @@ const Navigation = () => {
   return (
     <div className="navigation">
       <Link to="/maps">
-        <button className="button-css">MAPS</button>
+        <button>MAPS</button>
       </Link>
       <Link to="/database">
-        <button className="button-css">LICENSE DATABASE</button>
+        <button>LICENSE DATABASE</button>
       </Link>
       <Link to="/resources">
-        <button className="button-css">LICENSE RESOURCES</button>
+        <button>LICENSE RESOURCES</button>
       </Link>
     </div>
   );
