4ceae773803cf5c3341897ae017d28cd6c0687ba
Dan <78275188+rann143@users.noreply.github.com>
2025-06-05 13:06:53 -0400

    License Info Section - Spelling/Grammar Fixes (#88)

--- client/src/components/pages/database/license-type.tsx
+++ client/src/components/pages/database/license-type.tsx
@@ -13,7 +13,7 @@ const LicenseType = () => {
       <p className="mt-[8px] mb-[32px] font-normal text-[16px]">
         <FormattedMessage
           id="database-page.license-type.header2-paragraph"
-          defaultMessage="Boston has several different types of licenses from a variety of different laws and statutes, which can lead to some confusion when applying for a license. These licenses can range from those that can be applied city-wide to licenses for specific areas of Boston. Below are some basic definitions of each license type to aid you on your search."
+          defaultMessage="Boston has several different types of licenses from a variety of different laws and statutes, which can lead to some confusion when applying for a license. These licenses can range from those that apply city-wide to licenses for specific areas of Boston. Below are some basic definitions of each license type to aid you in your search."
         />
       </p>
       <h3 className="font-medium text-[32px]">
@@ -25,7 +25,7 @@ const LicenseType = () => {
       <p className="mt-[8px] mb-[32px] font-normal text-[16px]">
         <FormattedMessage
           id="database-page.license-type.transferable-paragraph"
-          defaultMessage="Licenses that have no restrictions preventing them from being transferred to another business and/or another part of the city of Boston. Most of the Licenses in Boston are made of this type and all Licenses issued before 2006 will fall under this category."
+          defaultMessage="Licenses that have no restrictions preventing them from being transferred to another business and/or another part of the city of Boston. Most of the licenses in Boston are of this type, and all licenses issued before 2006 fall under this category."
         />
       </p>
       <h3 className="font-medium text-[32px]">
@@ -37,7 +37,7 @@ const LicenseType = () => {
       <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
         <FormattedMessage
           id="database-page.license-type.nonTransferable-paragraph"
-          defaultMessage="Restrictions are placed on certain licenses preventing them from being transferred outside of their legally designated area of Boston. These licenses can come in several different forms, most notably the licenses created under the 2024 law designating Licenses to Several Zip Codes."
+          defaultMessage="Restrictions are placed on certain licenses, preventing them from being transferred outside of their legally designated area of Boston. These licenses can come in several different forms, most notably the licenses created under the 2024 law designating licenses to several zip codes."
         />
       </p>
       <h4 className="font-medium text-[24px]">
@@ -49,7 +49,7 @@ const LicenseType = () => {
       <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
         <FormattedMessage
           id="database-page.license-type.law-paragraph"
-          defaultMessage="In 2024 the city of Boston passed a new law to increase access to liquor licenses in several parts of Boston. Through this law the city will be issuing 5 new on-premise liquor licenses to 13 of Boston's Zip Codes each year for the next 3 years."
+          defaultMessage="In 2024, the city of Boston passed a new law to increase access to liquor licenses in several parts of Boston. Through this law, the city will issue 5 new on-premise liquor licenses to 13 of Boston's zip codes each year for the next 3 years."
         />
       </p>
       <h4 className="font-medium text-[24px]">
@@ -61,7 +61,7 @@ const LicenseType = () => {
       <p className="mt-[8px] font-normal text-[16px]">
         <FormattedMessage
           id="database-page.license-type.specialAreas-paragraph"
-          defaultMessage="Another provision of the 2024 law was the creation of specialty licenses for Oak Square in Brighton. Several other legislations in the past have also carved out licenses for specific areas in the city as well."
+          defaultMessage="Another provision of the 2024 law was the creation of specialty licenses for Oak Square in Brighton. Several other laws in the past have also carved out licenses for specific areas of the city as well."
         />
       </p>
     </section>
